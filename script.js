dc8f4b538d455ff55bd1ec66ea024429aa1b12ce
joao <jgsspinelli92@gmail.com>
2021-09-11 23:48:53 -0300

    refatora o segundo requisito e completa o requisito 6 e 7

--- script.js
+++ script.js
@@ -1,39 +1,25 @@
-// Requisito 02 e 03
-function createColorPalette() {
-  const parentElement = document.querySelector('#color-palette')
-
-  for(index = 0; index < 4; index += 1) {
-    if (index === 0) {
-      const square = document.createElement('div');
-      square.className = 'color';
-      square.style.backgroundColor = 'black';
-      parentElement.appendChild(square)
-    } else if (index === 1) {
-      const square = document.createElement('div');
-      square.className = 'color';
-      square.style.backgroundColor = 'green';
-      parentElement.appendChild(square)
-    } else if (index === 2) {
-      const square = document.createElement('div');
-      square.className = 'color';
-      square.style.backgroundColor = 'red';
-      parentElement.appendChild(square)
-    } else {
-      const square = document.createElement('div');
-      square.className = 'color';
-      square.style.backgroundColor = 'yellow';
-      parentElement.appendChild(square)
-    }
-  }
-}
-createColorPalette()
-// Requisito 04
 function createGrid(number) {
   const parentElement = document.querySelector('#pixel-board')
   for(let index = 0; index < number; index += 1) {
     const pixel = document.createElement('div');
-    pixel.className = 'pixel'
-    parentElement.appendChild(pixel)
+    pixel.className = 'pixel';
+    parentElement.appendChild(pixel);
   }
 }
 createGrid("25")
+// Requisito 06
+function activeColor() {
+  const black = document.getElementById('black')
+  const green = document.getElementById('green')
+  const purple = document.getElementById('purple')
+  const orange = document.getElementById('orange')
+
+  black.classList.add('selected')
+  console.log(black.classList)
+
+
+
+
+
+}
+activeColor()
